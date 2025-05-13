fa02475acc9d1129b2d337472f33f08581c929f7
Toru Komatsu <k0ma@utam0k.jp>
2025-05-14 07:04:00 +0900

    Show HierarchicalResourceQuota status by kubectl get (#17)
    Co-authored-by: @mochizuki875

--- test/e2e/hrq_test.go
+++ test/e2e/hrq_test.go
@@ -323,6 +323,47 @@ var _ = PDescribe("Hierarchical Resource Quota", Serial, func() {
 	})
 })
 
+var _ = Describe("HRQ Printer Columns Test", Serial, func() {
+	BeforeEach(func() {
+		rand.Seed(time.Now().UnixNano())
+		CleanupTestNamespaces()
+	})
+
+	AfterEach(func() {
+		CleanupTestNamespaces()
+	})
+
+	It("should show HRQ resource usage and limits in kubectl get hrq columns", func() {
+		// set up namespaces
+		CreateNamespace(nsA)
+		CreateSubnamespace(nsB, nsA)
+
+		// Set up HRQs with specific limits
+		setHRQ("printer-test-hrq", nsA, "memory", "200Mi", "cpu", "300m")
+
+		// Create resources that will show up in usage
+		createPod("printer-test-pod", nsB, "memory 100Mi cpu 150m", "memory 50Mi cpu 75m")
+
+		// Wait for resource usage to be recorded
+		FieldShouldContain("hrq", nsA, "printer-test-hrq", ".status.used", "memory:50Mi")
+		FieldShouldContain("hrq", nsA, "printer-test-hrq", ".status.used", "cpu:75m")
+
+		// Now verify that kubectl get hrq shows the columns with correct values
+		RunShouldContain("printer-test-hrq", propagationTime, "kubectl get hrq -n", nsA)
+		RunShouldContain("memory", propagationTime, "kubectl get hrq -n", nsA)
+		RunShouldContain("cpu", propagationTime, "kubectl get hrq -n", nsA)
+
+		// Verify the column headers are present
+		rawOutput, err := RunCommand("kubectl get hrq -n", nsA)
+		Expect(err).Should(BeNil())
+		// The default output should include our printer columns
+		Expect(rawOutput).Should(ContainSubstring("NAME"))
+		Expect(rawOutput).Should(ContainSubstring("REQUEST"))
+		Expect(rawOutput).Should(ContainSubstring("LIMIT"))
+		Expect(rawOutput).Should(ContainSubstring("AGE"))
+	})
+})
+
 func generateHRQManifest(nm, nsnm string, args ...string) string {
 	return `# temp file created by hrq_test.go
 apiVersion: hnc.x-k8s.io/v1alpha2
